204922599fa34800c2919d3e3290589957c5e211
sinsyhi <Thanapat00556@gmail.com>
2025-01-06 22:11:54 +0700

    Update code of file name in line 15 and update README.md

--- Doc_no_Excel.py
+++ Doc_no_Excel.py
@@ -5,12 +5,13 @@ def generate_text_range(start, end, prefix):
     numbers_array = [f"{prefix}{i:04d}" for i in range(start, end + 1)]
     return numbers_array
 
-prefix = input('Enter product number: ')
+station = input('Enter station name: ')
+prefix = input('Enter product number prefix: ')
 start = int(input('Enter start number: '))
 end = int(input('Enter end number: '))
 
 #Part of creating excel file
 dataframe = pd.DataFrame({'Product number': generate_text_range(start, end, prefix)}) #Create dataframe
-writer = pd.ExcelWriter(f'Product number of {prefix}.xlsx', engine='xlsxwriter') #Create excel file
+writer = pd.ExcelWriter(f'Product_number_List_{station}.xlsx', engine='xlsxwriter') #Create excel file
 dataframe.to_excel(writer, sheet_name='Sheet1', index=False) #Write dataframe to excel file
 writer.close() #Save excel file
